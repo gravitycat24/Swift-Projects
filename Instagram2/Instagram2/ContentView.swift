e119eeeba5dfd49405ed1cbc07996c209e92dbd6
gravitycat24 <78720498+gravitycat24@users.noreply.github.com>
2021-05-01 21:52:54 -0700

    Add files via upload

---
+++ Instagram2/Instagram2/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Instagram2
+//
+//  Created by Ben Risley on 2/8/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
